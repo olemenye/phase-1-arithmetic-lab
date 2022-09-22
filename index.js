e10592763279a763efd7688a7fc8de90e8483743
Moringa School <Abdulbasid.sheikh@student.moringaschool>
2022-09-22 16:14:41 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+// Write your code hereonst num1 = 62
+const num1 = 62
+const num2 = 1
+const multiply =num1 * num2;
+
+const random = num1;
+Math.random(num1);
+
+const num3 = 36;
+const num4 = 9;
+const mod = num3 / num4;
+
+let max = 20
+Math.max(1,2,3,4,20);
+
+
